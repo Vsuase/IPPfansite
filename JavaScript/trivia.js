9815195a92ce77d561ea3ab00c859b682acf7d90
Vsuase <rehansg.2024@gmail.com>
2024-11-22 09:21:16 +1030

    html, Js, css upload

---
+++ JavaScript/trivia.js
@@ -0,0 +1,25 @@
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const images = document.querySelectorAll('.masonry-layout img');
+
+    images.forEach((img) => {
+        gsap.fromTo(img, {
+            opacity: 0,
+            transform: "rotate3d(-2, 0, 0, 45deg)",
+        }, {
+            opacity: 1,
+            duration: 0.4,
+            transform: "rotate3d(0, 0, 0, 0)",
+            scrollTrigger: {
+                trigger: img,
+                start: "top 100%",
+                end: "top 20%",
+                toggleActions: "play none play none",
+                markers: false,
+                scrub: 0.2,
+            }
+        });
+    });
+});
